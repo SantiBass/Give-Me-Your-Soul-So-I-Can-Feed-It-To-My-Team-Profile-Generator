ea51f7e13ce8205470e6d1826c65866350a9732d
SantiBass <sibarraryan@gmail.com>
2021-12-02 11:35:14 -0800

    first-commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const inquirer = require('inquirer');
+const fs = require('fs')
+const Employees = ('./lib/Employees.js');
+const Manager = ('./lib/Manager.js');
+const Intern = ('./lib/Intern');
+const Egineer = ('./lib/Engeneer');
+console.log(Egineer)  
